cfc1ffd3436b4050320036123fac927a037791af
Brownie <euan_brown@asl.org>
2026-01-28 16:12:15 +0000

    Changes

--- combiner.py
+++ combiner.py
@@ -1,10 +1,11 @@
 f1 = open("mo.wav", "rb")
-#f2 = open("mo2.wav", "rb")
+f2 = open("SICKOMODE.wav", "rb")
 fw = open("combined.wav", "wb")
-'''
-# Read the first 44 bytes of the first file (WAV header)
+
 header1 = f1.read(44)
+data_pos = header1.find(b"data")
 header2 = f2.read(44)
+
 
 header1 = bytearray(header1)
 header2 = bytearray(header2)
@@ -13,10 +14,17 @@ size1 = int.from_bytes(header1[4:8], "little")
 size2 = int.from_bytes(header2[4:8], "little")
 
 new_size = size1 + size2
-header1[4:8] = new_size.to_bytes(4, "little")
-'''
-fade_samples = 44100 * 4 * 4
 
+new_riff_size = (size1 - 8) + (size2 - 8) + 8 #riff causing errors 
+header1[4:8] = new_riff_size.to_bytes(4, "little")
+
+new_data_size = (size1 - 44) + (size2 - 44)  
+header1[data_pos + 4 : data_pos + 8] = new_data_size.to_bytes(4, "little")
+
+fw.write(header1)
+
+fade_samples = 44100 * 4 * 2
+i=0
 while True:
     sample_bytes = f1.read(2)
     if not sample_bytes:
@@ -42,7 +50,7 @@ while True:
     fw.write(b)
 
 f1.close()
-'''
+
 #Make it so that the data size info and stuff is carried over into the first header
 for i in range(44):
     f2.read(1)
@@ -55,7 +63,7 @@ while True:
     fw.write(b)
 
 f2.close()
-'''
+
 fw.close()
 
 print("Hello World")
