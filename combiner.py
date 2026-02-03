8f2a2dab85040805f71d5e0d125f6f81e3be41e7
Brownie <euan_brown@asl.org>
2026-02-03 12:59:14 +0000

    Updates to conbiner

--- combiner.py
+++ combiner.py
@@ -2,23 +2,24 @@ f1 = open("mo.wav", "rb")
 f2 = open("SICKOMODE.wav", "rb")
 fw = open("combined.wav", "wb")
 
-header1 = f1.read(44)
-header2 = f2.read(44)
+header1 = f1.read(100)
+header2 = f2.read(100)
 
 header1 = bytearray(header1)
 header2 = bytearray(header2)
+data_pos1 = header1.find(b"data")
+data_pos2 = header2.find(b"data")
+overlapsize = 3
 #gets the file size
-size1 = int.from_bytes(header1[4:8], "little")
-size2 = int.from_bytes(header2[4:8], "little")
+datasize1 = int.from_bytes(header1[data_pos1+4:data_pos1+8], "little")
+datasize2 = int.from_bytes(header2[data_pos2+4:data_pos2+8], "little")
 
-datasize1 = int.from_bytes(header1[40:44],"little")
-datasize2 = int.from_bytes(header2[40:44],"little")
 newdatasize = datasize1+datasize2
 new_file_size = newdatasize + 36
 
 
 header1[4:8] = new_file_size.to_bytes(4, "little")
-header1[40:44] = newdatasize.to_bytes(4, "little")
+header1[data_pos1+4:data_pos1+8] = newdatasize.to_bytes(4, "little")
 
 fw.write(header1)
 
@@ -26,7 +27,7 @@ fade_samples = 44100 * 4 * 2
 i=0
 while True:
     sample_bytes = f1.read(2)
-    if not sample_bytes:
+    if datasize1 - 88000:
         break
     
     sample_int = int.from_bytes(sample_bytes, "little", signed=True)
@@ -47,8 +48,12 @@ for i in range(44):
     f2.read(1)
 
 
+#combiner
+for i in range(88000):
+    #write both f1 and f2 at same time for that 10 seconds from the datasize1 - 10 thing
+
 while True:
-    b = f2.read(1)
+    b = f2.read(2)
     if not b:
         break
     fw.write(b)
