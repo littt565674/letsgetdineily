8ed79efae52feda8fe7374f96c21de4ed1c54a49
Brownie <euan_brown@asl.org>
2026-02-05 10:37:51 +0000

    Changes to the basics ovwr overlap code

--- combiner.py
+++ combiner.py
@@ -17,49 +17,44 @@ datasize2 = int.from_bytes(header2[data_pos2+4:data_pos2+8], "little")
 newdatasize = datasize1+datasize2
 new_file_size = newdatasize + 36
 
+overlap = 88000 * 16
 
 header1[4:8] = new_file_size.to_bytes(4, "little")
 header1[data_pos1+4:data_pos1+8] = newdatasize.to_bytes(4, "little")
 
 fw.write(header1)
 
-fade_samples = 44100 * 4 * 2
-i=0
-while True:
-    sample_bytes = f1.read(2)
-    if datasize1 - 88000:
-        break
-    
-    sample_int = int.from_bytes(sample_bytes, "little", signed=True)
-    
-    if i < fade_samples:
-        factor = i / fade_samples
-        sample_faded = int(sample_int * factor)
-        fw.write(sample_faded.to_bytes(2, "little", signed=True))
-    else:
-        fw.write(sample_bytes)  # Just write original bytes
-    
-    i += 1
+data1 = f1.read(datasize1)
+data2 = f2.read(datasize2)
+
+fw.write(data1[:-overlap])
+
+for i in range(0, overlap):
+    if i % 2 == 0:
+        sample1 = int.from_bytes(data1[-overlap + i: -overlap + i + 2], "little")
+        sample2 = int.from_bytes(data2[i:i + 2], "little")
+        combined = sample1 + sample2
+        fw.write(combined.to_bytes(2, "little"))
+
+fw.write(data2[overlap:])
 
 f1.close()
-
+f2.close()
+fw.close()
 #Make it so that the data size info and stuff is carried over into the first header
-for i in range(44):
-    f2.read(1)
+#for i in range(44):
+ #   f2.read(1)
 
 
 #combiner
-for i in range(88000):
-    #write both f1 and f2 at same time for that 10 seconds from the datasize1 - 10 thing
+#while True:
+ #   b = f2.read(2)
+  #  if not b:
+   #     break
+    #fw.write(b)
 
-while True:
-    b = f2.read(2)
-    if not b:
-        break
-    fw.write(b)
+#f2.close()
 
-f2.close()
-
-fw.close()
+#fw.close()
 
 print("Hello World")
